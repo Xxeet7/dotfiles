0b5a5f555b5fc506bdfd72dbea12f405b1a4ab35
Xxeet7 <2good4u991@gmail.com>
2026-01-01 04:32:28 +0700

    docs(nvim): add link to dashboard for wakatime

--- nvim/lua/plugins/wakatime.lua
+++ nvim/lua/plugins/wakatime.lua
@@ -3,6 +3,7 @@
 -- ABOUT : WakaTime plugin for Neovim/Vim - automatic time tracking and metrics for programmers.
 -- LINKS : https://github.com/wakatime/vim-wakatime
 -- ================================================================================================
+-- check it here: https://wakatime.com/dashboard
 
 return {
   'wakatime/vim-wakatime',
