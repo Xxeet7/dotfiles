8de81493bab9174f68bf65626c76c2f4c8a42f0c
Xxeet7 <2good4u991@gmail.com>
2026-01-27 23:58:32 +0700

    feat(nvim): add diffview plugin nvim

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+-- ================================================================================================
+-- TITLE : diffview.nvim
+-- ABOUT : Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
+-- LINKS : https://github.com/sindrets/diffview.nvim
+-- ================================================================================================
+
+return {
+  "sindrets/diffview.nvim",
+  event = "VeryLazy",
+}
